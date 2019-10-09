b0d596beec839aecdf6d50664c395ba08c0d20b6
Geoffrey Lentner <glentner@purdue.edu>
2019-10-09 15:15:36 -0400

    added working /user/auth endpoint to web-api

--- refitt/apps/refitt/service_webapi.py
+++ refitt/apps/refitt/service_webapi.py
@@ -14,23 +14,118 @@
 
 # standard libs
 import os
-import io
 import sys
 import json
 import subprocess
 
 # internal libs
+from ... import database
 from ...core.logging import logger
-from ...core.config import config
-from ...database.client import DatabaseClient, ServerAddress, UserAuth
 from ...__meta__ import (__appname__, __copyright__, __developer__,
                          __contact__, __website__)
 
 # external libs
 from cmdkit.app import Application
 from cmdkit.cli import Interface
-from flask import Flask as FlaskServer
-from pandas import DataFrame, read_sql
+from flask import Flask, Response, request
+
+
+# flask application
+application = Flask(__name__)
+
+# response codes
+STATUS = {'Ok': 200,
+          'Forbidden': 403,
+          'Not Found': 404,
+          'Internal Server Error': 500,
+          'Service Unavailable': 503}
+
+
+@application.errorhandler(STATUS['Not Found'])
+def not_found(error=None) -> Response:
+    """Response to an invalid request."""
+    response = {'status': 'Not Found', }
+    return Response(json.dumps(response), status=200,
+                    mimetype='application/json')
+
+
+@application.route('/user/auth', methods=['GET'])
+def auth() -> Response:
+    """Request for a user's `authkey` and `authtoken`."""
+
+    args = dict(request.args)
+    response = {'status': 'success',
+                'query': {'endpoint': '/user/auth',
+                          'input': request.args}}
+
+    try:
+        # required query parameters
+        auth0_key = str(args.pop('auth_key'))
+        auth0_token = str(args.pop('auth_token'))
+        user_id = int(args.pop('user_id'))
+
+        if args:
+            raise ValueError('Invalid parameters', list(args.keys()))
+
+        # query database for credentials
+        auth0 = database.user.auth(auth_key=auth0_key, auth_valid=True)
+
+        if auth0.empty:
+            raise ValueError('Invalid or missing credentials.')
+
+        if auth0_token not in auth0.auth_token.values:
+            raise ValueError('Token is not valid.')
+
+        # select credential
+        auth0_chosen = auth0[auth0.auth_token == auth0_token].iloc[0]
+
+        # only 0-level auth can request credentials
+        if auth0_chosen.auth_level > 0:
+            raise ValueError('Level zero authorization required.')
+
+        # query for user credentials
+        user = database.user.auth(user_id=user_id, auth_valid=True)
+
+        if user.empty:
+            raise ValueError(f'No valid credentials for userid={user_id}.')
+
+        # select newest valid credentials and re-inject "auth_id" as named field
+        # NOTE: `numpy` types are not serializable by `json` (ergo, must be coerced back to Python types)
+        user_auth = user.reset_index().sort_values(by='auth_time', ascending=False).iloc[0].to_dict()
+        response['data'] = {'auth_id': int(user_auth['auth_id']),
+                            'auth_level': int(user_auth['auth_level']),
+                            'auth_key': str(user_auth['auth_key']),
+                            'auth_token': str(user_auth['auth_token']),
+                            'auth_valid': bool(user_auth['auth_valid']),
+                            'auth_time': str(user_auth['auth_time']),
+                            'user_id': int(user_auth['user_id'])}
+
+    except KeyError as error:
+        response['status'] = 'error'
+        response['message'] = f'Missing parameter: "{error.args[0]}"'
+
+    except ValueError as error:
+        response['status'] = 'error'
+        response['message'] = error.args
+
+    except Exception as error:
+        response['status'] = 'critical'
+        response['message'] = error.args
+
+    finally:
+        return Response(json.dumps(response), status=200,
+                        mimetype='application/json')
+
+
+@application.route('/observation/object_type', methods=['GET'])
+def observation_object_type() -> str:
+    """Query database for observation records."""
+
+    from io import StringIO
+
+    buffer = StringIO()
+    database.observation.object_types.to_json(buffer, orient='records')
+    return buffer.getvalue()
 
 
 # program name is constructed from module file name
@@ -69,26 +164,6 @@ options:
 
 # initialize module level logger
 log = logger.with_name(f'{__appname__}.{NAME}')
-
-# flask application
-application = FlaskServer(__name__)
-
-
-@application.route('/', methods=['GET'])
-def index() -> str:
-    return json.dumps({'status': 'ok', })
-
-@application.route('/observation/object_type', methods=['GET'])
-def observation_object_type() -> str:
-    """Query database for observation records."""
-
-    from ...database import observation
-    from io import StringIO
-
-    buffer = StringIO()
-    observation.object_types.to_json(buffer, orient='records')
-    return buffer.getvalue()
-
 
 
 class WebAPIApp(Application):
--- refitt/database/queries.py
+++ refitt/database/queries.py
@@ -12,6 +12,73 @@
 
 """Set of database query templates."""
 
+USER = """\
+SELECT
+    "User"."UserID"                as "user_id",
+    "User"."UserName"              as "user_name",
+    "User"."UserProfile"           as "user_profile",
+    "User"."UserLatitude"          as "user_latitude",
+    "User"."UserLongitude"         as "user_longitude",
+    "User"."UserElevation"         as "user_elevation",
+    "User"."UserLimitingMagnitude" as "user_limiting_magnitude"
+
+FROM
+    "User"."User" as "User"
+"""
+
+USER_WHERE_USER_ID = """\
+    "User"."UserID" = {user_id}
+"""
+
+USER_WHERE_USER_NAME = """\
+    "User"."UserName" = {user_name}
+"""
+
+# TODO: a better scheme for doing comparative filtering (e.g., range based).
+
+
+AUTH = """\
+SELECT
+    "Auth"."AuthID"    as "auth_id",
+    "Auth"."AuthLevel" as "auth_level",
+    "Auth"."AuthKey"   as "auth_key",
+    "Auth"."AuthToken" as "auth_token",
+    "Auth"."AuthValid" as "auth_valid",
+    "Auth"."AuthTime"  as "auth_time",
+    "Auth"."UserID"    as "user_id"
+
+FROM
+    "User"."Auth" as "Auth"
+"""
+
+AUTH_WHERE_AUTH_ID = """\
+    "Auth"."AuthID" = {auth_id}
+"""
+
+AUTH_WHERE_AUTH_LEVEL = """\
+    "Auth"."AuthLevel" = {auth_level}
+"""
+
+AUTH_WHERE_AUTH_KEY = """\
+    "Auth"."AuthKey" = '{auth_key}'
+"""
+
+AUTH_WHERE_AUTH_TOKEN = """\
+    "Auth"."AuthToken" = '{auth_token}'
+"""
+
+AUTH_WHERE_AUTH_VALID = """\
+    "Auth"."AuthValid" = {auth_valid}
+"""
+
+AUTH_WHERE_AUTH_TIME = """\
+    "Auth"."AuthTime" = '{auth_time}'
+"""
+
+AUTH_WHERE_USER_ID = """\
+    "Auth"."UserID" = {user_id}
+"""
+
 OBJECT_TYPE = """\
 SELECT *
 FROM "Observation"."ObjectType"
@@ -19,31 +86,33 @@ FROM "Observation"."ObjectType"
 
 OBJECT = """\
 SELECT
-	"Object"."ObjectID"           AS "ObjectID",
-	"Object"."ObjectName"         AS "ObjectName",
-	"Object"."ObjectRA"           AS "ObjectRA",
-	"Object"."ObjectDec"          AS "ObjectDec",
-	"Object"."ObjectRedShift"     AS "ObjectRedShift",
-	"ObjectType"."ObjectTypeName" AS "ObjectTypeName"
+    "Object"."ObjectID"           AS "object_id",
+    "Object"."ObjectName"         AS "object_name",
+    "Object"."ObjectRA"           AS "object_ra",
+    "Object"."ObjectDec"          AS "object_dec",
+    "Object"."ObjectRedShift"     AS "object_redshift",
+    "ObjectType"."ObjectTypeName" AS "object_typename"
 
 FROM
-	"Observation"."Object" AS "Object"
+    "Observation"."Object" AS "Object"
 
 LEFT JOIN
-	"Observation"."ObjectType" AS "ObjectType"
-	ON "Object"."ObjectTypeID" = "ObjectType"."ObjectTypeID"
+    "Observation"."ObjectType" AS "ObjectType"
+    ON "Object"."ObjectTypeID" = "ObjectType"."ObjectTypeID"
 """
 
 OBJECT_WHERE_OBJECTTYPE = """\
-	"Object"."ObjectTypeID"
-	IN (
-		SELECT "ObjectType"."ObjectTypeID"
-		FROM "Observation"."ObjectType" as "ObjectType"	
-		WHERE "ObjectTypeName" LIKE '{object_type}'
+    "Object"."ObjectTypeID"
+    IN (
+        SELECT
+            "ObjectType"."ObjectTypeID"
+        FROM
+            "Observation"."ObjectType" as "ObjectType"
+        WHERE
+            "ObjectTypeName" LIKE '{object_type}'
     )
 """
 
 OBJECT_WHERE_OBJECTRA = """\
-	"Object"."ObjectRA" {OP} {VALUE}
+    "Object"."ObjectRA" {OP} {VALUE}
 """
-
