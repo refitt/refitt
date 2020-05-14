f6ce54687823b56ded0af6a636e33a5d01769d75
Geoffrey Lentner <glentner@purdue.edu>
2020-05-14 13:42:23 -0400

    add entirely new api implementation

--- refitt/web/api/__init__.py
+++ refitt/web/api/__init__.py
@@ -11,3 +11,156 @@
 # If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
 
 """REFITT's REST-API implementation."""
+
+# type annotations
+from __future__ import annotations
+
+# standard libs
+import json
+
+# external libs
+from flask import Flask, Response, request
+
+# internal libs
+from ...database.auth import Client
+from .response import STATUS, restful
+from .auth import authenticate, authenticated, authorization
+from .endpoints import token, profile
+from .logging import logged
+
+# flask application
+application = Flask(__name__)
+
+
+@application.errorhandler(STATUS['Not Found'])
+def not_found(error=None) -> Response:
+    """Response to an invalid request."""
+    return Response(json.dumps({'status': 'error',
+                                'message': f'not found: {request.path}'}),
+                    status=STATUS['Not Found'],
+                    mimetype='application/json')
+
+
+@application.route('/token', methods=['GET'])
+@logged
+@restful
+@authenticate
+def get_token(client: Client) -> dict:
+    return {'access': token.get(client.client_id)}
+
+
+@application.route('/token/<int:user_id>', methods=['GET'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def get_user_token(client: Client, user_id: int) -> dict:
+    return {'access': token.get_user(user_id)}
+
+
+@application.route('/profile/user', methods=['POST'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def post_user_profile(client: Client) -> dict:
+    return {'profile': profile.user.post(request.json)}
+
+
+@application.route('/profile/user/<int:user_id>', methods=['GET'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def get_user_profile(client: Client, user_id: int) -> dict:
+    return {'profile': profile.user.get(user_id)}
+
+
+@application.route('/profile/user/<int:user_id>', methods=['PUT'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def put_user_profile(client: Client, user_id: int) -> dict:
+    return {'profile': profile.user.put(user_id, request.json)}
+
+
+@application.route('/profile/user/<int:user_id>', methods=['DELETE'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def delete_user_profile(client: Client, user_id: int) -> dict:
+    return {'profile': profile.user.delete(user_id)}
+
+
+@application.route('/profile/facility', methods=['POST'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def post_facility_profile(client: Client) -> dict:
+    return {'profile': profile.facility.post(request.json)}
+
+
+@application.route('/profile/facility/<int:facility_id>', methods=['GET'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def get_facility_profile(client: Client, facility_id: int) -> dict:
+    return {'profile': profile.facility.get(facility_id)}
+
+
+@application.route('/profile/facility/<int:facility_id>', methods=['PUT'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def put_facility_profile(client: Client, facility_id: int) -> dict:
+    return {'profile': profile.facility.put(facility_id, request.json)}
+
+
+@application.route('/profile/facility/<int:facility_id>', methods=['DELETE'])
+@logged
+@restful
+@authenticated
+@authorization(level=0)
+def delete_facility_profile(client: Client, facility_id: int) -> dict:
+    return {'profile': profile.facility.delete(facility_id)}
+
+
+@application.route('/recommendation', methods=['GET'])
+@logged
+@restful
+@authenticated
+@authorization(level=None)
+def get_recommendation(client: Client) -> dict:
+    raise NotImplementedError(request.path)
+
+
+@application.route('/recommendation/<int:recommendation_id>', methods=['GET'])
+@logged
+@restful
+@authenticated
+@authorization(level=None)
+def get_single_recommendation(client: Client, recommendation_id) -> dict:
+    raise NotImplementedError(request.path)
+
+
+@application.route('/recommendation/<int:recommendation_id>', methods=['POST'])
+@logged
+@restful
+@authenticated
+@authorization(level=None)
+def post_recommendation(client: Client, recommendation_id) -> dict:
+    raise NotImplementedError(request.path)
+
+
+@application.route('/recommendation/<int:recommendation_id>/<string:action>', methods=['PUT'])
+@logged
+@restful
+@authenticated
+@authorization(level=None)
+def put_recommendation(client: Client, recommendation_id, action) -> dict:
+    raise NotImplementedError(request.path)
---
+++ refitt/web/api/auth.py
@@ -0,0 +1,85 @@
+# Copyright REFITT Team 2019. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the Apache License (v2.0) as published by the Apache Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the Apache License for more details.
+#
+# You should have received a copy of the Apache License along with this program.
+# If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
+
+"""REFITT's REST-API authentication/authorization."""
+
+# type annotations
+from typing import Callable
+
+# standard libs
+from functools import wraps
+from datetime import datetime
+
+# external libs
+from flask import request
+
+# internal libs
+from .exceptions import AuthorizationNotFound, AuthorizationInvalid
+from ...database.auth import (Secret, JWT, Client, TokenExpired, TokenNotFound,
+                              ClientInsufficient, ClientInvalid)
+
+
+def authenticate(route: Callable[[Client], dict]) -> Callable[[Client], dict]:
+    """Check key:secret authorization in request."""
+
+    @wraps(route)
+    def get_client() -> dict:
+        """Lookup/validate credentials and pass to route."""
+        if not request.authorization:
+            raise AuthorizationNotFound('expected key:secret authorization')
+        client = Client.from_key(request.authorization.username)
+        secret = Secret(request.authorization.password)
+        if secret != client.client_secret:
+            raise AuthorizationInvalid('client secret was invalid')
+        return route(client)
+
+    return get_client
+
+
+def authenticated(route: Callable[..., dict]) -> Callable[..., dict]:
+    """Check `request` headers for valid token."""
+
+    @wraps(route)
+    def get_client(*args, **kwargs) -> dict:
+        """Validate token and lookup client credentials."""
+
+        header = request.headers.get('Authorization', None)
+        prefix = 'Bearer '
+        if header is None or not header.startswith(prefix):
+            raise TokenNotFound('expected "Authorization: Bearer <token>" in header')
+
+        token = JWT.decrypt(header[len(prefix):].strip().encode())
+        if token.exp is not None and token.exp > datetime.utcnow():
+            raise TokenExpired('token expired')
+
+        client = Client.from_id(token.sub)
+        return route(client, *args, **kwargs)
+
+    return get_client
+
+
+def authorization(level: int = None) -> Callable:
+    """Validate client access and privilege level."""
+
+    def valid(route: Callable[..., dict]) -> Callable[..., dict]:
+        """Ensure client is valid."""
+
+        @wraps(route)
+        def check_access(client: Client, *args, **kwargs) -> dict:
+            if not client.client_valid:
+                raise ClientInvalid('access has been revoked')
+            if level is not None and client.client_level > level:
+                raise ClientInsufficient('access level insufficient')
+            return route(client, *args, **kwargs)
+
+        return check_access
+    return valid
