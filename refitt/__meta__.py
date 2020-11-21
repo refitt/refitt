4cb4b5416c02fef85d5d2fb97f335c3902bef1b6
Geoffrey Lentner <glentner@purdue.edu>
2020-11-20 19:17:40 -0500

    Alter __authors__ to be list

--- refitt/__meta__.py
+++ refitt/__meta__.py
@@ -12,21 +12,20 @@
 
 """Package metadata for refitt."""
 
+
 __appname__     = 'refitt'
 __version__     = '0.11.4'
-__authors__     = """\
-Dan Milisavljevic, Ph.D. <dmilisav@purdue.edu>;
-Niharika Sravan, Ph.D. <nsravan@purdue.edu>;
-Geoffrey Lentner, M.Sc. <glentner@purdue.edu>;
-Mark Linvill <mlinvill@purdue.edu>
-"""
+__authors__     = ['Dan Milisavljevic, Ph.D. <dmilisav@purdue.edu>',
+                   'Niharika Sravan, Ph.D. <nsravan@purdue.edu>',
+                   'Geoffrey Lentner, M.Sc. <glentner@purdue.edu>',
+                   'Mark Linvill <mlinvill@purdue.edu>']
 __developer__   = 'Geoffrey Lentner'
 __contact__     = '<glentner@purdue.edu>'
 __license__     = 'Apache License 2.0'
 __website__     = 'https://github.com/refitt/refitt'
-__copyright__   = 'REFITT Team 2019.'
+__copyright__   = 'REFITT Team 2019'
 __description__ = 'The Recommender Engine for Intelligent Transient Tracking.'
-__keywords__    = 'astronomy science machine-learning service'
+__keywords__    = 'astronomy science machine-learning recommendation service'
 __ascii_art__   = r"""
     ____  ____________________________
    / __ \/ ____/ ____/  _/_  __/_  __/
