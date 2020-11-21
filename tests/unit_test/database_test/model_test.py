16a16a600c75f91899077f8e9ad7d9ca15adaa67
Geoffrey Lentner <glentner@purdue.edu>
2020-11-20 20:36:50 -0500

    Add super user to test suite

--- tests/unit_test/database_test/model_test.py
+++ tests/unit_test/database_test/model_test.py
@@ -73,34 +73,26 @@ class TestUser:
         for user in testdata['user']:
             assert User.from_alias(user['alias']).alias == user['alias']
 
-    def test_add_remove(self) -> None:
-        """Add a new user record and then remove it."""
-        assert User.count() == 3
-        User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
-                  'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
-        assert User.count() == 4
-        assert User.from_alias('007').last_name == 'Bond'
-        User.delete(User.from_alias('007').id)
-        assert User.count() == 3
-
     def test_update_email(self) -> None:
         """Update email address of user profile."""
         old_email, new_email = 'bourne@cia.gov', 'jason.bourne@cia.gov'
-        assert User.from_id(1).email == old_email
-        User.update(1, email=new_email)
-        assert User.from_id(1).email == User.from_email(new_email).email
-        User.update(1, email=old_email)
-        assert User.from_id(1).email == User.from_email(old_email).email
+        user = User.from_alias('delta_one')
+        assert user.email == old_email
+        User.update(user.id, email=new_email)
+        assert User.from_alias('delta_one').email == User.from_email(new_email).email
+        User.update(user.id, email=old_email)
+        assert User.from_alias('delta_one').email == User.from_email(old_email).email
 
     def test_update_data(self) -> None:
         """Update custom data of user profile."""
         old_data = {'user_type': 'amateur'}
         new_data = {'user_type': 'amateur', 'special_field': 42}
-        assert User.from_alias('tomb_raider').data == old_data
-        User.update(2, special_field=42)
-        assert User.from_alias('tomb_raider').data == new_data
-        User.update(2, data=old_data)
-        assert User.from_alias('tomb_raider').data == old_data
+        user_id = User.from_alias('tomb_raider').id
+        assert User.from_id(user_id).data == old_data
+        User.update(user_id, special_field=42)
+        assert User.from_id(user_id).data == new_data
+        User.update(user_id, data=old_data)
+        assert User.from_id(user_id).data == old_data
 
     def test_facilities(self) -> None:
         """Access associated user facilities."""
@@ -125,8 +117,18 @@ class TestUser:
         assert len(facilities) == 2 and set(f.name for f in facilities) == {'Croft_1m', 'Croft_4m'}
 
     def test_delete(self) -> None:
+        """Add a new user record and then remove it."""
+        assert User.count() == 4
+        User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
+                  'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
+        assert User.count() == 5
+        assert User.from_alias('007').last_name == 'Bond'
+        User.delete(User.from_alias('007').id)
+        assert User.count() == 4
+
+    def test_delete_facility_map_cascade(self) -> None:
         """Create a new user, with facility, then remove."""
-        assert User.count() == 3 and Facility.count() == 4 and FacilityMap.count() == 4
+        assert User.count() == 4 and Facility.count() == 4 and FacilityMap.count() == 4
         User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
                   'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
         user = User.from_alias('007')
@@ -135,9 +137,9 @@ class TestUser:
         facility = Facility.from_name('Bond_4m')
         user.add_facility(facility.id)
         assert user.facilities()[0].to_dict() == facility.to_dict()
-        assert User.count() == 4 and Facility.count() == 5 and FacilityMap.count() == 5
+        assert User.count() == 5 and Facility.count() == 5 and FacilityMap.count() == 5
         User.delete(user.id)
-        assert User.count() == 3 and Facility.count() == 5 and FacilityMap.count() == 4
+        assert User.count() == 4 and Facility.count() == 5 and FacilityMap.count() == 4
         Facility.delete(facility.id)
         assert Facility.count() == 4
 
@@ -179,16 +181,6 @@ class TestFacility:
         """Test loading facility profile from `name`."""
         for facility in testdata['facility']:
             assert Facility.from_name(facility['name']).name == facility['name']
-
-    def test_delete(self) -> None:
-        """Add a new facility record and then delete it."""
-        assert Facility.count() == 4
-        Facility.add({'name': 'Croft_10m', 'latitude': -25.5, 'longitude': -69.25, 'elevation': 5050,
-                      'limiting_magnitude': 20.5})
-        assert Facility.count() == 5
-        assert Facility.from_name('Croft_10m').limiting_magnitude == 20.5
-        Facility.delete(Facility.from_name('Croft_10m').id)
-        assert Facility.count() == 4
 
     def test_update_limiting_magnitude(self) -> None:
         """Update limiting magnitude of facility profile."""
@@ -232,17 +224,27 @@ class TestFacility:
         users = facility.users()
         assert len(users) == 1 and users[0].alias == 'tomb_raider'
 
+    def test_delete(self) -> None:
+        """Add a new facility record and then delete it."""
+        assert Facility.count() == 4
+        Facility.add({'name': 'Croft_10m', 'latitude': -25.5, 'longitude': -69.25, 'elevation': 5050,
+                      'limiting_magnitude': 20.5})
+        assert Facility.count() == 5
+        assert Facility.from_name('Croft_10m').limiting_magnitude == 20.5
+        Facility.delete(Facility.from_name('Croft_10m').id)
+        assert Facility.count() == 4
+
     def test_delete_facility_map_cascade(self) -> None:
         """Create a new facility, associate it with a user, then remove."""
-        assert User.count() == 3 and Facility.count() == 4 and FacilityMap.count() == 4
+        assert User.count() == 4 and Facility.count() == 4 and FacilityMap.count() == 4
         Facility.add({'name': 'Bourne_4m', 'latitude': -25.5, 'longitude': -69.25, 'elevation': 5050,
                       'limiting_magnitude': 17.5, 'data': {'telescope_design': 'reflector'}})
         facility = Facility.from_name('Bourne_4m')
         user = User.from_alias('delta_one')
         user.add_facility(facility.id)
-        assert User.count() == 3 and Facility.count() == 5 and FacilityMap.count() == 5
+        assert User.count() == 4 and Facility.count() == 5 and FacilityMap.count() == 5
         Facility.delete(facility.id)
-        assert User.count() == 3 and Facility.count() == 4 and FacilityMap.count() == 4
+        assert User.count() == 4 and Facility.count() == 4 and FacilityMap.count() == 4
 
 
 class TestClient:
@@ -274,16 +276,16 @@ class TestClient:
 
     def test_embedded(self) -> None:
         """Test embedded method to check JSON-serialization and auto-join."""
-        assert Client.from_id(1).embedded() == {
-            "id": 1,
-            "user_id": 1,
-            "level": 5,
+        assert Client.from_user(User.from_alias('delta_one').id).embedded() == {
+            "id": 2,
+            "user_id": 2,
+            "level": 10,
             "key": "78h6IuhW30Re7I-C",
             "secret": "7ccb08b171f4a28e6b5f2af5597153873d7cd90a972f2bee7b8ac82c43e0e4e9",
             "valid": True,
             "created": "2020-10-23 17:45:01-04:00",
             "user": {
-                "id": 1,
+                "id": 2,
                 "first_name": "Jason",
                 "last_name": "Bourne",
                 "email": "bourne@cia.gov",
@@ -305,6 +307,11 @@ class TestClient:
         for id in range(1, 4):
             assert id == Client.from_user(id).user_id == User.from_id(id).id
 
+    def test_from_key(self, testdata: TestData) -> None:
+        """Test loading client from `key`."""
+        for client in testdata['client']:
+            assert Client.from_key(client['key']).key == client['key']
+
     def test_relationship_user(self) -> None:
         """Test user foreign key relationship."""
         for id in range(1, 4):
@@ -312,44 +319,46 @@ class TestClient:
 
     def test_delete(self) -> None:
         """Add a new user and client. Remove the client directly."""
-        assert User.count() == 3 and Client.count() == 3
+        assert User.count() == 4 and Client.count() == 4
         user_id = User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
                             'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
-        assert User.count() == 4 and Client.count() == 3
+        assert User.count() == 5 and Client.count() == 4
         key, secret, client = Client.new(user_id)
-        assert User.count() == 4 and Client.count() == 4
+        assert User.count() == 5 and Client.count() == 5
         Client.delete(client.id)
-        assert User.count() == 4 and Client.count() == 3
+        assert User.count() == 5 and Client.count() == 4
         User.delete(user_id)
-        assert User.count() == 3 and Client.count() == 3
+        assert User.count() == 4 and Client.count() == 4
 
     def test_delete_user_cascade(self) -> None:
         """Add a new user and client record and then remove them."""
-        assert User.count() == 3 and Client.count() == 3
+        assert User.count() == 4 and Client.count() == 4
         user_id = User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
                             'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
-        assert User.count() == 4 and Client.count() == 3
+        assert User.count() == 5 and Client.count() == 4
         Client.new(user_id)
-        assert User.count() == 4 and Client.count() == 4
+        assert User.count() == 5 and Client.count() == 5
         User.delete(user_id)
-        assert User.count() == 3 and Client.count() == 3
+        assert User.count() == 4 and Client.count() == 4
 
     def test_new_secret(self) -> None:
         """Generate a new client secret and then manually reset it back."""
-        old_hash = Client.from_id(1).secret
-        new_hash = Client.new_secret(1).hashed().value
+        user = User.from_alias('tomb_raider')
+        old_hash = Client.from_user(user.id).secret
+        new_hash = Client.new_secret(user.id).hashed().value
         assert new_hash != old_hash
-        Client.update(1, secret=old_hash)
-        assert Client.from_id(1).secret == old_hash
+        Client.update(Client.from_user(user.id).id, secret=old_hash)
+        assert Client.from_user(user.id).secret == old_hash
 
     def test_new_key_and_secret(self) -> None:
         """Generate a new client key and secret and then manually reset them."""
-        data = Client.from_id(1).to_dict()
+        user = User.from_alias('tomb_raider')
+        data = Client.from_user(user.id).to_dict()
         old_key, old_secret_hash = data['key'], data['secret']
-        key, secret = Client.new_key(1)
+        key, secret = Client.new_key(user.id)
         assert key.value != old_key and secret.hashed().value != old_secret_hash
-        Client.update(1, key=old_key, secret=old_secret_hash)
-        client = Client.from_id(1)
+        Client.update(Client.from_key(key.value).id, key=old_key, secret=old_secret_hash)
+        client = Client.from_user(user.id)
         assert client.key == old_key and client.secret == old_secret_hash
 
 
@@ -382,20 +391,20 @@ class TestSession:
 
     def test_embedded(self) -> None:
         """Test embedded method to check JSON-serialization and auto-join."""
-        assert Session.from_id(1).embedded() == {
-            'id': 1,
-            'client_id': 1,
+        assert Session.from_id(2).embedded() == {
+            'id': 2,
+            'client_id': 2,
             'expires': '2020-10-23 18:00:01-04:00',
             'token': 'c44d20d18e734aea40b30682a57162b53c18f676c1b752696dad5dc6586187fe',
             'created': '2020-10-23 17:45:01-04:00',
-            'client': {'id': 1,
-                       'user_id': 1,
-                       'level': 5,
+            'client': {'id': 2,
+                       'user_id': 2,
+                       'level': 10,
                        'key': '78h6IuhW30Re7I-C',
                        'secret': '7ccb08b171f4a28e6b5f2af5597153873d7cd90a972f2bee7b8ac82c43e0e4e9',
                        'valid': True,
                        'created': '2020-10-23 17:45:01-04:00',
-                       'user': {'id': 1,
+                       'user': {'id': 2,
                                 'first_name': 'Jason',
                                 'last_name': 'Bourne',
                                 'email': 'bourne@cia.gov',
@@ -424,47 +433,47 @@ class TestSession:
 
     def test_delete(self) -> None:
         """Add a new session and remove it directly."""
-        assert User.count() == 3 and Client.count() == 3 and Session.count() == 3
+        assert User.count() == 4 and Client.count() == 4 and Session.count() == 4
         user_id = User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
                             'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
-        assert User.count() == 4 and Client.count() == 3 and Session.count() == 3
+        assert User.count() == 5 and Client.count() == 4 and Session.count() == 4
         key, secret, client = Client.new(user_id)
-        assert User.count() == 4 and Client.count() == 4 and Session.count() == 3
+        assert User.count() == 5 and Client.count() == 5 and Session.count() == 4
         Session.new(user_id)
-        assert User.count() == 4 and Client.count() == 4 and Session.count() == 4
+        assert User.count() == 5 and Client.count() == 5 and Session.count() == 5
         Session.delete(Session.from_client(client.id).id)
-        assert User.count() == 4 and Client.count() == 4 and Session.count() == 3
+        assert User.count() == 5 and Client.count() == 5 and Session.count() == 4
         User.delete(user_id)  # NOTE: deletes client
-        assert User.count() == 3 and Client.count() == 3 and Session.count() == 3
+        assert User.count() == 4 and Client.count() == 4 and Session.count() == 4
 
     def test_delete_client_cascade(self) -> None:
         """Add a new user, client, and session. Remove user to clear client and session."""
-        assert User.count() == 3 and Client.count() == 3 and Session.count() == 3
+        assert User.count() == 4 and Client.count() == 4 and Session.count() == 4
         user_id = User.add({'first_name': 'James', 'last_name': 'Bond', 'email': 'bond@secret.gov.uk',
                             'alias': '007', 'data': {'user_type': 'amateur', 'drink_of_choice': 'martini'}})
-        assert User.count() == 4 and Client.count() == 3 and Session.count() == 3
+        assert User.count() == 5 and Client.count() == 4 and Session.count() == 4
         Client.new(user_id)
-        assert User.count() == 4 and Client.count() == 4 and Session.count() == 3
+        assert User.count() == 5 and Client.count() == 5 and Session.count() == 4
         Session.new(user_id)
-        assert User.count() == 4 and Client.count() == 4 and Session.count() == 4
+        assert User.count() == 5 and Client.count() == 5 and Session.count() == 5
         User.delete(user_id)
-        assert User.count() == 3 and Client.count() == 3 and Session.count() == 3
+        assert User.count() == 4 and Client.count() == 4 and Session.count() == 4
 
     def test_new_token(self) -> None:
         """Generate a new client secret and then manually reset it back."""
-        session = Session.from_client(1)
+        session = Session.from_client(2)
         before = session.created
         assert datetime.now().astimezone() > before
         old_hash = session.token
         expired = session.expires
-        jwt = Session.new(1)
+        jwt = Session.new(2)
         assert isinstance(jwt, JWT)
         assert jwt.exp > datetime.now()
-        new_session = Session.from_client(1)
+        new_session = Session.from_client(2)
         assert new_session.created > before
         assert new_session.token != old_hash and len(new_session.token) == len(old_hash)
-        Session.update(1, token=old_hash, created=before, expires=expired)  # NOTE: hard reset
-        new_session = Session.from_client(1)
+        Session.update(2, token=old_hash, created=before, expires=expired)  # NOTE: hard reset
+        new_session = Session.from_client(2)
         assert new_session.created == before
         assert new_session.token == old_hash
         assert new_session.expires == expired
