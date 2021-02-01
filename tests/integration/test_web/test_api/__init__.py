1c7c4f65f8128b3649f4499c14e01958a12487f7
Geoffrey Lentner <glentner@purdue.edu>
2021-02-01 16:12:49 -0500

    Add restore_client context manager for API tests

--- tests/integration/test_web/test_api/__init__.py
+++ tests/integration/test_web/test_api/__init__.py
@@ -64,5 +64,14 @@ def restore_session(client_id: int) -> None:
     try:
         yield
     finally:
-        Session.update(session.id, token=session.token,
-                       expires=session.expires, created=session.created)
+        Session.update(session.id, token=session.token, expires=session.expires, created=session.created)
+
+
+@contextmanager
+def restore_client(client_id: int) -> None:
+    """Force restore token for session for given client."""
+    client = Client.from_id(client_id)
+    try:
+        yield
+    finally:
+        Client.update(client.id, key=client.key, secret=client.secret, created=client.created)
