2760b65042e12617ca0aba4d31ff7b15d807c1e5
Geoffrey Lentner <glentner@purdue.edu>
2021-03-19 23:23:17 -0400

    Version 0.13.0

--- refitt/__meta__.py
+++ refitt/__meta__.py
@@ -14,7 +14,7 @@
 
 
 __appname__     = 'refitt'
-__version__     = '0.12.2'
+__version__     = '0.13.0'
 __authors__     = ['Dan Milisavljevic, Ph.D. <dmilisav@purdue.edu>',
                    'Niharika Sravan, Ph.D. <nsravan@purdue.edu>',
                    'Geoffrey Lentner, M.Sc. <glentner@purdue.edu>',
