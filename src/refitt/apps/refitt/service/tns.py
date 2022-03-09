321b045fa8c6109f4fbedc1b86938dc9992ce6b0
Geoffrey Lentner <glentner@purdue.edu>
2022-03-09 00:18:59 -0500

    Update frontmatter (copyright year)

--- src/refitt/apps/refitt/service/tns.py
+++ src/refitt/apps/refitt/service/tns.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2019-2021 REFITT Team
+# SPDX-FileCopyrightText: 2019-2022 REFITT Team
 # SPDX-License-Identifier: Apache-2.0
 
 """Query Transient Name Server for object info."""
